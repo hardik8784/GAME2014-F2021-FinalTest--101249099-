e1f797026509e30ac4a2a83cd709ae6d1c325d7e
Hardik <hardik8784@gmail.com>
2021-12-15 09:42:31 -0500

    Added Shrinking and Expanding Functionality

--- Assets/[Scripts]/FloatingPlatformController.cs
+++ Assets/[Scripts]/FloatingPlatformController.cs
@@ -5,8 +5,9 @@
  * File : FloatingPlatformController.cs
  * Description : This is the script for the Floating and Shrinking Platforms.
  * Revision History : v0.1 > Added Same Behaviour and Mechanics as Moving Platforms
+ *                    v0.2 > Added Shrinking and Expanding Functionality implemented with Player's Events
+ *                    v0.3 > Check Player Collides with the Platform then Shrink otherwise Expand,if not both then Float
  */
-
 
 using System.Collections;
 using System.Collections.Generic;
@@ -24,23 +25,125 @@ public class FloatingPlatformController : MonoBehaviour
 
     private Vector3 distance;
 
+    [Header("Shrinking Behaviour of Platform")]
+    [SerializeField] private float ShrinkTimeElapsed;
+    [Range(0.1f, 10.0f)]
+    [SerializeField] private float ShrinkTime;
+    [SerializeField] private Vector3 Platform_Position;
+  
+    /// <summary>
+    /// Scaling properties for the Platform
+    /// </summary>
+    private Vector3 TempScale;
+    private Vector2 BoxColliderScale;
+
+    [Range(0.0f, 1.0f)]
+    [SerializeField] float ScalingFactor;
+    private BoxCollider2D BoxCollider;
+
+    [Header("Platform Behaviour")]
+    [SerializeField] private bool isActive;
+    [SerializeField] private bool isExpanding;
+
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerBehaviour>();
+
+        BoxCollider = GetComponent<BoxCollider2D>();
+        Platform_Position = transform.position;
+        BoxColliderScale = BoxCollider.size;
         platformTimer = 0.1f;
         platformTimer = 0;
+        isActive = false;
+        isExpanding = false;
         distance = end.position - start.position;
+        ShrinkTimeElapsed = ShrinkTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        platformTimer += Time.deltaTime;
-        _Move();    
+        if (!isActive)
+        {
+            if (!isExpanding)
+            {
+                platformTimer += Time.deltaTime;
+                Float();
+            }
+            else
+            {
+                Expand();
+            }
+        }
+        else
+        {
+            Shrink();
+        }
     }
 
-    private void _Move()
+    /// <summary>
+    /// The Platform will Shrink if Player is on it
+    /// </summary>
+    void Shrink()
+    {
+        ShrinkTimeElapsed -= Time.deltaTime;
+        ScalingFactor = ShrinkTimeElapsed / ShrinkTime;
+
+        TempScale = transform.localScale;
+        TempScale.x = ScalingFactor;
+        TempScale.y = ScalingFactor;
+        transform.localScale = TempScale;
+
+        Vector2 NewBoxScale = BoxColliderScale * ScalingFactor;
+        BoxCollider.size = NewBoxScale;
+
+    }
+
+    /// <summary>
+    /// The Platform will Expand if the Player is not on it.
+    /// </summary>
+    void Expand()
+    {
+        ShrinkTimeElapsed += Time.deltaTime;
+        ScalingFactor = ShrinkTimeElapsed / ShrinkTime;
+
+
+        TempScale = transform.localScale;
+        TempScale.x = ScalingFactor;
+        TempScale.y = ScalingFactor;
+        transform.localScale = TempScale;
+       
+        Vector2 NewBoxScale = BoxColliderScale * ScalingFactor;
+        BoxCollider.size = NewBoxScale;
+
+        if (ScalingFactor >= 1.0f)
+        {
+            ScalingFactor = 1.0f;
+            isExpanding = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)  //Check the Player and Platform Collision
+    {
+
+        if (player != null)
+        {
+            isActive = true;
+            isExpanding = false;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)   ////Check the Player move somewhere else from Platform 
+    {
+        isActive = false;
+        isExpanding = true;
+
+    }
+
+
+    private void Float()
     {
         var distanceX = (distance.x > 0) ? start.position.x + Mathf.PingPong(platformTimer, distance.x) : start.position.x;
         var distanceY = (distance.y > 0) ? start.position.y + Mathf.PingPong(platformTimer, distance.y) : start.position.y;
